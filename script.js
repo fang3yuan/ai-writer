2ad88edc2c7e3e4e043a1e8ea59c50fb778d76e2
fang3yuan <obaidatos470@gmail.com>
2025-04-12 16:46:23 +0300

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const toggle = document.getElementById('toggleTheme');
-  toggle.addEventListener('click', () => {
-    document.body.classList.toggle('dark-mode');
-  });
-});
