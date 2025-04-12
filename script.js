a3be3f808b31901f9b25bfc0a27faf34f11ecd26
fang3yuan <obaidatos470@gmail.com>
2025-04-12 16:52:00 +0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function sendToAI() {
-    const inputText = document.getElementById('inputText').value;
-    document.getElementById('response').innerText = "AI Response: " + inputText;
-}
-
-function generateImage() {
-    const imageDescription = document.getElementById('imageInput').value;
-    document.getElementById('imageResult').innerText = "Generated Image for: " + imageDescription;
-}
